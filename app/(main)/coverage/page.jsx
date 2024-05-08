dc4e58553e88c05268256c5010801d038a60d4ce
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2024-05-08 14:08:51 +0530

    updated coverage page for mobile screen

--- app/(main)/coverage/page.jsx
+++ app/(main)/coverage/page.jsx
@@ -67,12 +67,12 @@ const Coverage = () => {
           </button>
         </div>
       </div>
-      <div className="mb-[210px] flex justify-center items-start gap-[80px] mx-10 mt-[100px] h-fit flex-wrap-reverse md:mx-auto md:mb-[210px] md:flex-nowrap xl:gap-[80px]">
-        <div className="pb-auto basis-full max-w-[639px] xl:basis-[45%] md:basis-[40%]">
-          <h2 className="mb-6 text-2xl leading-[60.48px] font-semibold text-[#2E3342] md:text-5xl">
+      <div className="mb-[88px] flex justify-center items-start gap-x-[80px] gap-y-[26px] mx-0 mt-[100px] h-fit flex-wrap-reverse md:mb-[210px] md:mx-auto md:flex-nowrap xl:gap-[80px]">
+        <div className="pb-auto mx-5 basis-full max-w-[639px] md:mx-0 md:basis-[40%] xl:basis-[45%]">
+          <h2 className="mb-6 text-2xl leading-[30.24px] font-semibold text-[#2E3342] md:text-5xl md:leading-[60.48px]">
             We have a range of risk you can insure against
           </h2>
-          <p className="w-[90%] text-[18px] lading-[26.1px] font-normal text-[#2E3342]">
+          <p className="text-sm leading-[20.3px] font-normal text-[#2E3342] md:w-[90%] md:text-lg md:leading-[26.1px]">
             We are dedicated to making the process of buying and claiming
             insurance transparent, easy, and accessible through our innovative
             digital platform.  <br /> We  envision a future where safeguarding
@@ -89,7 +89,7 @@ const Coverage = () => {
               alt="rick management"
             />
             <svg
-              className="absolute w-[50px] top-[30px] -left-[16%] sm:w-[80px] sm:h-[80px] sm:top-[45px] sm:-left-[20%] xl:w-[135px] xl:top-[60px] xl:-left-[22%] xl:h-[135px]"
+              className="overflow-visible absolute w-[50px] top-[30px] -left-[16%] sm:w-[80px] sm:h-[80px] sm:top-[45px] sm:-left-[20%] xl:w-[135px] xl:top-[60px] xl:-left-[23%] xl:h-[135px]"
               viewBox="0 0 135 135"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -98,12 +98,27 @@ const Coverage = () => {
                 d="M134.153 1.31286C134.133 0.576753 133.52 -0.00385528 132.784 0.0160396C132.048 0.0359345 131.467 0.648799 131.487 1.3849C131.507 2.12102 132.12 2.70163 132.856 2.68173C133.592 2.66184 134.173 2.04897 134.153 1.31286ZM2.83284 133.464C2.81294 132.728 2.20008 132.147 1.46397 132.167C0.727867 132.187 0.147259 132.8 0.167154 133.536C0.187048 134.272 0.799913 134.853 1.53602 134.833C2.27212 134.813 2.85273 134.2 2.83284 133.464ZM1.49824 1.34889L1.49824 1.09889L1.24824 1.09888L1.24824 1.34888L1.49824 1.34889ZM1.49823 72.9998L1.24823 72.9998L1.24823 72.9998L1.49823 72.9998ZM132.82 1.09888L1.49824 1.09889L1.49823 1.59889L132.82 1.59888L132.82 1.09888ZM1.24824 1.34888L1.24823 72.9998L1.74823 72.9998L1.74824 1.34889L1.24824 1.34888ZM1.24823 72.9998L1.25 133.5L1.74999 133.5L1.74823 72.9998L1.24823 72.9998Z"
                 fill="#AF060D"
               />
+              <rect
+                x="-25"
+                y="145"
+                fill="#FEE6E7"
+                rx="12"
+                ry="12"
+                className="rounded-2xl w-[40px] h-[18px] md:w-[46px] md:h-[25px]"
+              />
+              <text
+                x="-12"
+                y="162"
+                className="fill-custom-red bg-[#FEE6E7] -translate-y-[5px] text-[10px] leading-[12.1px] md:translate-0 md:text-sm"
+              >
+                fire
+              </text>
             </svg>
             <svg
-              className="absolute w-[50px] top-[13px] -right-[45px] sm:w-[80px] sm:h-[129px] sm:top-[92px] sm:-right-[70px] xl:top-[128px] xl:-right-[100px] xl:w-[113px] xl:h-[182px]"
+              className="absolute w-[60px] top-[24px] -right-[55px] sm:w-[80px] sm:h-[129px] sm:top-[92px] sm:-right-[70px] xl:top-[128px] xl:-right-[100px] xl:w-[113px] xl:h-[182px]"
               width="113"
               height="182"
-              viewBox="0 0 113 182"
+              viewBox="0 0 125 210"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
@@ -111,15 +126,30 @@ const Coverage = () => {
                 d="M0.863113 2.40294C1.22966 3.04161 2.04454 3.26222 2.68321 2.89568C3.32189 2.52913 3.54249 1.71425 3.17595 1.07557C2.80941 0.436903 1.99452 0.216298 1.35585 0.58284C0.717176 0.949382 0.496571 1.76427 0.863113 2.40294ZM110.344 181.164C110.71 181.802 111.525 182.023 112.164 181.656C112.802 181.29 113.023 180.475 112.656 179.836C112.29 179.198 111.475 178.977 110.836 179.344C110.198 179.71 109.977 180.525 110.344 181.164ZM111.5 1.73926L111.75 1.73926L111.75 1.48925L111.5 1.48926L111.5 1.73926ZM111.5 85.9997L111.25 85.9997L111.5 85.9997ZM2.01953 1.98926L111.5 1.98926L111.5 1.48926L2.01953 1.48926V1.98926ZM111.25 1.73926L111.25 85.9997L111.75 85.9997L111.75 1.73926L111.25 1.73926ZM111.25 85.9997L111.25 180.5L111.75 180.5L111.75 85.9997L111.25 85.9997Z"
                 fill="#AF060D"
               />
+              <rect
+                x="48"
+                y="185"
+                fill="#FEE6E7"
+                rx="12"
+                ry="12"
+                className="w-[70px] h-[18px] rounded-2xl translate-x-[5px] md:w-[79px] md:h-[25px]"
+              />
+              <text
+                x="60"
+                y="202"
+                className="fill-custom-red translate-x-[10px] -translate-y-[5px] bg-[#FEE6E7] text-[10px] leading-[12.1px] md:text-sm"
+              >
+                burglary
+              </text>
             </svg>
           </div>
         </div>
       </div>
-      <div className="mb-[206px]">
-        <h2 className="mb-2 text-[40px] text-center leading-[50.4px] font-bold text-custom-grey">
+      <div className="mb-[88px] mx-5 md:mb-[206px]">
+        <h2 className="mb-2 text-2xl leading-[30.24px] font-bold text-custom-grey md:text-[40px] md:leading-[50.4px] md:text-center">
           Benefits of coverage
         </h2>
-        <p className="mb-[51px] text-lg text-center leading-[27px] font-normal text-custom-grey">
+        <p className="mb-[51px] text-sm leading-[21px] font-normal text-custom-grey md:text-center md:text-lg md:leading-[27px]">
           Having coverage for fire/natural disasters and burglary offers several
           key benefits
         </p>
@@ -190,11 +220,11 @@ const Coverage = () => {
       <div className="mx-5 sm:mx-10 md:mx-auto">
         <h2
           className="mb-6 font-semibold text-2xl leading-[30.24px] text-[
-#2E3342] text-center md:text-[40px] md:leading-[50.4px]"
+#2E3342] md:text-[40px] md:leading-[50.4px] md:text-center"
         >
           Coverage options
         </h2>
-        <p className="mb-[60px] max-w-[500px] mx-auto text-base leading-[20.3px] text-center font-normal md:text-lg md:leading-[26.1px] xl:max-w-[866px]">
+        <p className="mb-12 max-w-[500px] mx-auto text-sm leading-[20.3px] font-normal md:mb-[60px] md:text-lg md:leading-[26.1px] md:text-center xl:max-w-[866px]">
           We are dedicated to making the process of buying and claiming
           insurance transparent, easy, and accessible through our innovative
           digital platform. 
@@ -203,17 +233,18 @@ const Coverage = () => {
           <TabSection />
           <div className="max-w-[331px] h-[487px] bg-[#FAFAFA] rounded-3xl overflow-hidden md:max-w-[573px] md:h-[794px]">
             <div className="relative w-[331px] h-[487px] xl:w-[573px] xl:h-[794px]">
-            <Image
-              className="shadow-[0_57.82_88.14_0_rgba(0 0 0/15)"
-              src="/coverage/tab-option-image-1.png"
-              sizes="100vw"
-              fill
-              alt="coverage detail"
-            />
+              <Image
+                className="shadow-[0_57.82_88.14_0_rgba(0 0 0/15)"
+                src="/coverage/tab-option-image-1.png"
+                sizes="100vw"
+                fill
+                alt="coverage detail"
+              />
             </div>
           </div>
         </div>
-        <div className="mx-5 my-[88px] pt-[54px] lg:mx-[258px] md:mt-[200px] md:mb-[207px] md:mx-20 md:pt-[54px] md:pb-20">
+      </div>
+      <div className="mx-5 my-[88px] pt-[54px] lg:mx-[258px] md:mt-[200px] md:mb-[207px] md:mx-20 md:pt-[54px] md:pb-20">
           <h2 className="mb-2 text-custom-grey text-2xl leading-[30.24px] font-semibold md:mb-10 md:leading-[50.4px] md:text-[40px] md:text-center">
             Frequently asked questions
           </h2>
@@ -223,7 +254,6 @@ const Coverage = () => {
           </p>
           <Accordion accordionItems={faq} />
         </div>
-      </div>
     </main>
   );
 };
